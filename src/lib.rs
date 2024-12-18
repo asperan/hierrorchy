dfc6865841ed39975fb7b68780dfdd54afea0311
Alex Speranza <alex.speranza@studio.unibo.it>
2024-12-18 22:20:41 +0100

    feat: add error_node proc macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use proc_macro::TokenStream;
-use quote::quote;
-use syn::{parse::Parse, parse_macro_input, ItemStruct, LitStr, Macro};
+use proc_macro2::{Group, TokenStream as TokenStream2};
+use quote::{format_ident, quote, ToTokens};
+use syn::{parse::Parse, parse_macro_input, Ident, ItemStruct, LitStr, Macro, Token};
 
 enum MessageFormat {
     Lit(LitStr),
@@ -83,4 +84,188 @@ pub fn error_leaf(attr: TokenStream, item: TokenStream) -> TokenStream {
     };
 
     result_stream.into()
+}
+
+struct ErrorNode {
+    is_pub: bool,
+    node_name: Ident,
+    variants: Vec<Ident>,
+    message_prefix: Option<LitStr>,
+}
+
+impl Parse for ErrorNode {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let is_pub = input.lookahead1().peek(Token![pub]);
+        if is_pub {
+            let _: Token![pub] = input.parse()?;
+        }
+
+        let _: Token![type] = input.parse()?;
+        let node_name: Ident = input.parse()?;
+
+        let mut variants: Vec<Ident> = vec![];
+        let _open_angle_bracket: Token![<] = input.parse()?;
+        let mut keep_parsing_variants = true;
+        while keep_parsing_variants {
+            if input.lookahead1().peek(Token![>]) {
+                keep_parsing_variants = false;
+                let _close_angle_bracket: Token![>] = input.parse()?;
+            } else {
+                variants.push(input.parse()?);
+                if input.lookahead1().peek(Token![,]) {
+                    let _: Token![,] = input.parse()?;
+                }
+            }
+        }
+
+        if input.is_empty() {
+            Ok(ErrorNode {
+                is_pub,
+                node_name,
+                variants,
+                message_prefix: None,
+            })
+        } else {
+            let _: Token![=] = input.parse()?;
+            let message_prefix: LitStr = input.parse()?;
+            Ok(ErrorNode {
+                is_pub,
+                node_name,
+                variants,
+                message_prefix: Some(message_prefix),
+            })
+        }
+    }
+}
+
+fn format_variant_name(number: usize) -> Ident {
+    format_ident!("Variant{}", number)
+}
+
+fn error_node_enum(node_name: &Ident, is_pub: bool, variants: &[Ident]) -> TokenStream {
+    let mut token_buffer = TokenStream2::new();
+    token_buffer.extend(quote! { #[derive(Debug)] });
+    if is_pub {
+        token_buffer.extend(quote! { pub });
+    }
+    token_buffer.extend(quote! { enum });
+    token_buffer.extend(node_name.clone().into_token_stream());
+    token_buffer.extend(
+        Group::new(
+            proc_macro2::Delimiter::Brace,
+            TokenStream2::from_iter(variants.iter().enumerate().map(|it| {
+                let variant_ident = format_variant_name(it.0);
+                let variant_inner_type = it.1;
+                quote! {
+                    #variant_ident(#variant_inner_type),
+                }
+            })),
+        )
+        .to_token_stream(),
+    );
+    token_buffer.into()
+}
+
+fn error_node_display_impl(node_name: &Ident, message_prefix: Option<&LitStr>) -> TokenStream {
+    let mut token_buffer = TokenStream2::new();
+    token_buffer.extend(quote! { impl std::fmt::Display for #node_name });
+    let message_format = format!(
+        "{}: {{}}",
+        match message_prefix {
+            Some(l) => l.value(),
+            None => node_name.to_string(),
+        }
+    );
+    let expect_message = format!("{} always has a source", node_name);
+    token_buffer.extend(
+        Group::new(
+            proc_macro2::Delimiter::Brace,
+            quote! {
+                fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                    write!(f, #message_format, &self.source().expect(#expect_message))
+                }
+            },
+        )
+        .to_token_stream(),
+    );
+    token_buffer.into()
+}
+
+fn error_node_error_impl(node_name: &Ident, variants: &[Ident]) -> TokenStream {
+    let mut token_buffer = TokenStream2::new();
+    token_buffer.extend(quote! { impl std::error::Error for #node_name });
+    let variant_matches = TokenStream2::from_iter(variants.iter().enumerate().map(|it| {
+        let variant_name = format_variant_name(it.0);
+        quote! {
+            Self::#variant_name(err) => Some(err),
+        }
+    }));
+    token_buffer.extend(
+        Group::new(
+            proc_macro2::Delimiter::Brace,
+            quote! {
+                fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+                    match self {
+                        #variant_matches
+                    }
+                }
+            },
+        )
+        .to_token_stream(),
+    );
+    token_buffer.into()
+}
+
+fn error_node_from_impls(node_name: &Ident, variants: &[Ident]) -> TokenStream {
+    let mut token_buffer = TokenStream2::new();
+    token_buffer.extend(variants.iter().enumerate().map(|it| {
+        let variant_inner_type = it.1;
+        let variant_name = format_variant_name(it.0);
+        quote! {
+            impl From<#variant_inner_type> for #node_name {
+                fn from(value: #variant_inner_type) -> Self {
+                    Self::#variant_name(value)
+                }
+            }
+        }
+    }));
+    token_buffer.into()
+}
+
+/// Function-like proc macro to construct error nodes.
+/// The body requires the following format:
+/// `type (name)<variants> [= (string)]`
+/// where `name` is the name to give to the error node (an enum), `variants` is a comma-separated list of other
+/// errors (both leaves and nodes), and `string` is an optional string to use rather than the node
+/// name when printing the error node.
+///
+/// # Examples:
+/// ```
+/// use error_tree::{error_leaf, error_node};
+/// use std::error::Error;
+///
+/// #[error_leaf(format!("error child 1"))]
+/// pub struct ErrorChild1 {}
+///
+/// error_node! { type MyErrorNode<ErrorChild1> = "custom prefix" }
+/// ```
+#[proc_macro]
+pub fn error_node(tokens: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(tokens as ErrorNode);
+    let is_pub = input.is_pub;
+    let node_name = input.node_name;
+    let variants = input.variants;
+    let message_prefix = input.message_prefix;
+
+    let enum_declaration = error_node_enum(&node_name, is_pub, &variants);
+    let impl_display = error_node_display_impl(&node_name, message_prefix.as_ref());
+    let impl_error = error_node_error_impl(&node_name, &variants);
+    let impl_froms = error_node_from_impls(&node_name, &variants);
+
+    let mut token_buffer = TokenStream::new();
+    token_buffer.extend(enum_declaration);
+    token_buffer.extend(impl_display);
+    token_buffer.extend(impl_error);
+    token_buffer.extend(impl_froms);
+    token_buffer
 }
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,4 +1,9 @@
-use error_tree::error_leaf;
+use std::error::Error;
+use error_tree::{error_leaf, error_node};
+
+error_node! {
+    type MyErrorNode<ErrorChild1,> = "custom message"
+}
 
 #[error_leaf(format!("error child 1"))]
 struct ErrorChild1 {}
